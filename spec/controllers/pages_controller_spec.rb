77f483632684d13f7c7809b518a3ba9e181fc578
Sam Chung <chungsa@1278035CSSLap.tacoma.washington.edu>
2011-08-28 22:50:49 -0700

    Iteration 6

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,7 +7,11 @@ describe PagesController do
     it "should be successful" do
       get 'home'
       response.should be_success
-      get 'home' 
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should  have_selector("title", :content => "Home")
     end
   end
 
@@ -16,12 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should  have_selector("title", :content => "Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success 
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should  have_selector("title", :content => "About")
     end
   end
 end
